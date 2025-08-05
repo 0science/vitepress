da3321642e068c69ded43ad19ada8088eed7a81a
djm <1415847612@qq.com>
2025-08-05 23:48:00 +0800

    Updates

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  ignoreDeadLinks: true,
+  title: "赛博空间",
+  description: "A VitePress Site",
+  themeConfig: {
+    // logo
+    logo: '/assets/logo.jpg',
+    // 头部导航栏
+    nav: [
+      { text: '简介', link: '/Introduce/' }
+    ],
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+    outline: [2, 6],
+    outlineTitle: '页面导航',
+    darkModeSwitchLabel: '切换主题',
+    sidebarMenuLabel: '菜单',
+    returnToTopLabel: '回到顶部',
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    }
+  }
+})
